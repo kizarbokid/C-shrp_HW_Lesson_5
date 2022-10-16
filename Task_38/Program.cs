42e23050557a7b01511cb5ed029b068f89d962f7
YakovkinVD <dikobrazzzik709@gmail.com>
2022-10-16 11:39:48 +0900

    Task_38 solved

---
+++ Task_38/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Clear();
+int input()
+{
+    Console.Write("Введите размерность массива: ");
+    string len = Console.ReadLine();
+    return int.Parse(len);
+}
+float[] create_rand_arr(int n)
+{
+    var rand = new Random();
+    float[] arr = new float[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand.NextSingle() * rand.Next(-100, 101);
+        Console.Write($"_{arr[i]}_");
+    }
+    Console.WriteLine();
+    return arr;
+}
+int n = input();
+float[] arr = create_rand_arr(n);
+
+float max = arr[0];
+float min = arr[0];
+for (int i = 0; i < n; i++)
+{
+    if (arr[i] > max)
+    {
+        max=arr[i];
+    }
+        if (arr[i] < min)
+    {
+        min=arr[i];
+    }
+}
+
+Console.WriteLine($"Разность {max} и {min} = {max-min};");
